cb481400c0ed3697d61d39d8873759ee37b63587
MJ <code.za@proton.me>
2023-02-25 16:37:29 +0200

    Update app.js

--- app.js
+++ app.js
@@ -19,23 +19,31 @@ app.set('views');
 const accessJsonFileData = require('./data.json');
 const { projects } = accessJsonFileData;
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-});
-
+//route handling for the about page
 app.get('/about', (req, res) => {
     res.render('about', {title: 'About'});
   });
 
+//route handling for the index page
 app.get("/", (req, res) => {
   res.render("index", { projects });
 });
 
-app.get('/projects/:id', (req, res) => {
+//route handling for each project
+app.get('/projects/:id', (req, res, next) => {
   
     const { id } = req.params;
     const indexId = parseInt(id) -1;
+
+    //check if the id is a number and if it is within the range of the projects array - if not, throw error
+    if (isNaN(indexId) || indexId < 0 || indexId >= projects.length) {
+      const err = new Error(`Project '${id}' does not exist`);
+      err.status = 404;
+      //return statement required to stop the below code from running, if true
+      return next(err);
+    }
+
+    // Store project array details in separate variables for easier access in pug template
     const project_name = projects[indexId].project_name;
     const description = projects[indexId].description;
     const technologies = projects[indexId].technologies;
@@ -43,19 +51,25 @@ app.get('/projects/:id', (req, res) => {
     const image_urls = projects[indexId].image_urls;
     const live_link = projects[indexId].live_link;
 
-  res.render("project", { indexId,project_name, description, technologies, github_link, image_urls,live_link, projects });
+    res.render("project", { indexId,project_name, description, technologies, github_link, image_urls,live_link, projects });
 });
 
-//Error Handling
-
+//Middleware 404 error handler
 app.use((req,res,next) => {
   const err = new Error('The page you were looking for does not exist.');
   err.status = 404;
-  console.log(err.status);
   next(err);
 });
 
+//Middleware global error handler
 app.use((err, req, res, next) => {
   res.status(err.status || 500);
   res.send(`<h1>Error ${err.status}</h1><p>` + err.message + '</p>');
+  console.log(err.message);
+});
+
+//start the server
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
